5a4cfa514b892265c4fba2d1625f35836c95216b
Gaëtan FEREZ <gaetan@ferez.fr>
2018-12-05 19:48:12 +0100

    Update postgresql hosts in settings

--- cyborgbackup/settings.py
+++ cyborgbackup/settings.py
@@ -134,7 +134,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME', 'cyborgbackup'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
         'PORT': os.environ.get('POSTGRES_PORT', 5432),
     }
 }
