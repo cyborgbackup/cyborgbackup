869ef99fbeeff2d96bd4ad3175b00f2aaaf3e0ac
Gaëtan FEREZ <gaetan@ferez.fr>
2019-02-10 11:40:21 +0100

    Add readable size in notification

--- src/cyborgbackup/main/tasks.py
+++ src/cyborgbackup/main/tasks.py
@@ -64,6 +64,24 @@ Try upgrading OpenSSH or providing your private key in an different format. \
 
 logger = logging.getLogger('cyborgbackup.main.tasks')
 
+def humanbytes(B):
+    'Return the given bytes as a human friendly KB, MB, GB, or TB string'
+    B = float(B)
+    KB = float(1024)
+    MB = float(KB ** 2) # 1,048,576
+    GB = float(KB ** 3) # 1,073,741,824
+    TB = float(KB ** 4) # 1,099,511,627,776
+ 
+    if B < KB:
+        return '{0} {1}'.format(B,'Bytes' if 0 == B > 1 else 'Byte')
+    elif KB <= B < MB:
+        return '{0:.2f} KB'.format(B/KB)
+    elif MB <= B < GB:
+        return '{0:.2f} MB'.format(B/MB)
+    elif GB <= B < TB:
+        return '{0:.2f} GB'.format(B/GB)
+    elif TB <= B:
+        return '{0:.2f} TB'.format(B/TB)
 
 def build_report(type):
     since = 24*60*60
@@ -93,8 +111,8 @@ def build_report(type):
                 'status': job.status,
                 'duration': str(datetime.timedelta(seconds=float(job.elapsed))),
                 'numberFiles': str(numberOfFiles),
-                'original_size': str(job.original_size),
-                'deduplicated_size': str(job.deduplicated_size)
+                'original_size': str(humanbytes(job.original_size)),
+                'deduplicated_size': str(humanbytes(job.deduplicated_size))
             }
             lines.append(line)
     report = {
