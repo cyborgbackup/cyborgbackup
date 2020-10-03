66c6e690d9ba3a052a16262cbc09189bb309ee1c
Gaëtan FEREZ <gaetan@ferez.fr>
2020-10-03 14:46:10 +0200

    Add little system status page for API System

--- src/cyborgbackup/main/utils/tasks.py
+++ src/cyborgbackup/main/utils/tasks.py
@@ -1,8 +1,12 @@
 # Python
 import logging
 
+from django.conf import settings
+
 # Celery
-from celery import Task, shared_task
+from celery import Task, shared_task, current_app
+
+from elasticsearch import Elasticsearch
 
 # CyBorgBackup
 from cyborgbackup.main.utils.task_manager import TaskManager
@@ -14,6 +18,23 @@ class LogErrorsTask(Task):
     def on_failure(self, exc, task_id, args, kwargs, einfo):
         logger.exception('Task {} encountered exception.'.format(self.name), exc_info=exc)
         super(LogErrorsTask, self).on_failure(exc, task_id, args, kwargs, einfo)
+
+
+def catalog_is_running():
+    try:
+        es_conf = settings.ELASTICSEARCH_DSL['default']['hosts'].split(':')
+        es = Elasticsearch([{'host': es_conf[0], 'port': int(es_conf[1])}], max_retries=0)
+        es.cluster.state()
+        return True
+    except Exception:
+        return False
+
+
+def celery_worker_is_running():
+    if len(current_app.control.ping()) > 0:
+        return True
+    else:
+        return False
 
 
 @shared_task(base=LogErrorsTask)
--- src/cyborgbackup/ui/urls.py
+++ src/cyborgbackup/ui/urls.py
@@ -3,6 +3,10 @@ from django.views.generic.base import TemplateView
 from django.contrib.staticfiles import views
 from django.urls import re_path
 
+
+from cyborgbackup.main.utils.tasks import catalog_is_running, celery_worker_is_running
+from cyborgbackup.main.models import Job, Policy, Repository, Schedule, Client
+
 app_name = 'ui'
 
 
@@ -12,7 +16,13 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        # Add any additional context info here.
+        context['celery'] = celery_worker_is_running()
+        context['catalog'] = catalog_is_running()
+        context['jobs'] = len(Job.objects.filter())
+        context['policies'] = len(Policy.objects.filter())
+        context['clients'] = len(Client.objects.filter())
+        context['schedules'] = len(Schedule.objects.filter())
+        context['repositories'] = len(Repository.objects.filter())
         return context
 
 
