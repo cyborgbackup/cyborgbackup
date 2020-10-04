73c2f90764bab2f217d7f17a79bd1f3c01d4c7e7
Gaëtan FEREZ <gaetan@ferez.fr>
2020-10-04 12:45:07 +0200

    Fix space in variable on tools scripts

--- tools/scripts/initcyborg.sh
+++ tools/scripts/initcyborg.sh
@@ -10,7 +10,7 @@ mkdir -p /opt/cyborgbackup/var/run
 
 python3 $HOME/manage.py migrate
 if [ -z "$CYBORG_READY" ]; then
-    python3 $HOME/manage.py loaddata settings
-    echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin@cyborg.local', 'admin')" | python3 $HOME/manage.py shell
+    python3 "$HOME/manage.py" loaddata settings
+    echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin@cyborg.local', 'admin')" | python3 "$HOME/manage.py" shell
     echo "export CYBORG_READY=1" >> /opt/cyborgbackup/.env
 fi
