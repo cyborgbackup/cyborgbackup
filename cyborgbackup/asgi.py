3ee491077ffaa7bb5616358f7a229cb03e174525
Gaëtan FEREZ <gaetan@ferez.fr>
2024-06-13 12:02:31 +0200

    Fix ASGI Daphne start

--- cyborgbackup/asgi.py
+++ cyborgbackup/asgi.py
@@ -5,6 +5,8 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
 
+django_asgi_app = get_asgi_application()
+
 from cyborgbackup import prepare_env
 from cyborgbackup.main.consumers import CyBorgBackupConsumer, JwtAuthMiddlewareStack
 
