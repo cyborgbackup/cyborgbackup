ba625d7abdec147db76a7082ebbd15d07b10e59b
Gaëtan FEREZ <gaetan@ferez.fr>
2024-12-20 10:44:01 +0100

    Add check for DB on postinst

--- tools/scripts/initcyborg.sh
+++ tools/scripts/initcyborg.sh
@@ -8,6 +8,18 @@ pip3 install --upgrade wheel
 pip3 install -r /usr/share/cyborgbackup/requirements.txt
 mkdir -p /opt/cyborgbackup/var/run
 
+check_postgresql() {
+    echo "Check PostgreSQL connexion"
+    PGPASSWORD="$POSTGRES_PASSWORD" psql -h "127.0.0.1" -U "cyborgbackup" -d "cyborgbackup" -c "\q" 2>/dev/null
+    return $?
+}
+
+if ! check_postgresql; then
+    echo "Error: PostgreSQL is not available. Please check database is started"
+    echo "Installation success with warning."
+    exit 0
+fi
+
 python3 "$HOME/manage.py" migrate
 python3 "$HOME/manage.py" collectstatic
 if [ -z "$CYBORG_READY" ]; then
--- tools/scripts/makedb.sh
+++ tools/scripts/makedb.sh
@@ -2,6 +2,18 @@
 
 . /opt/cyborgbackup/.env
 
+check_postgresql() {
+    echo "Check PostgreSQL connexion"
+    su - postgres -c 'psql -c "\du"' 2>/dev/null
+    return $?
+}
+
+if ! check_postgresql; then
+    echo "Error: PostgreSQL is not available. Please check database is started"
+    echo "Installation success with warning."
+    exit 0
+fi
+
 su - postgres -c 'psql -c "\du"' | grep -q 'cyborgbackup' 2>&1 1>/dev/null
 if [ $? -ne 0 ]; then
     echo "Create CyBorgBackup PostgreSQL User"
